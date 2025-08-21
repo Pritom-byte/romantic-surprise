55d815efd8149bd08c8be49ce55d5712e171fd61
Pritom-byte <pritommazumder1@gmail.com>
2025-08-21 23:19:31 +0200

    Auto update Thu Aug 21 23:19:31 CEST 2025

---
+++ auto_push.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+cd  /Users/skinnyfiend/Document/Pritom\'s\ Project/romantic-surprise    # replace with the full path to your project
+git add .
+git commit -m "Auto update $(date)"
+git push
