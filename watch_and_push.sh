c96b527fbc5f48e5e105315a856eede657dedc96
Pritom-byte <pritommazumder1@gmail.com>
2025-08-21 23:27:55 +0200

    Auto update Thu Aug 21 23:27:55 CEST 2025

--- watch_and_push.sh
+++ watch_and_push.sh
@@ -2,7 +2,11 @@
 # Watch the current folder for changes
 fswatch -0 . | while read -d "" event
 do
-    git add .
-    git commit -m "Auto update $(date)" 2>/dev/null
-    git push
+    # Check if there are changes
+    if [[ -n $(git status --porcelain) ]]; then
+        git add .
+        git commit -m "Auto update $(date)"
+        git push
+        echo "Changes pushed at $(date)"
+    fi
 done
